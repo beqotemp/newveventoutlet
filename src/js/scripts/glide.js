0e77b5f8f9ce3be714dc8b363e19ca274b102cac
beqotemp <godunovofc@gmail.com>
2020-02-18 22:36:36 +0300

    denis soset moy hui

--- src/js/scripts/glide.js
+++ src/js/scripts/glide.js
@@ -5,33 +5,32 @@ for (var i = 0; i < sliders.length; i++) {
   var glide = new Glide(sliders[i], {
     type: 'carousel',
 	startAt: 0,
-	perView: 4,
-    
+	perView: 1,
+ 	responsive: [
+    {
+      // screens greater than >= 775px
+      breakpoint: 775,
+      settings: {
+        // Set to `auto` and provide item width to adjust to viewport
+        slidesToShow: 'auto',
+        slidesToScroll: 'auto',
+        itemWidth: 150,
+        duration: 0.25
+      }
+    },{
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25
+      }
+    }
+  ]
   });
   
   glide.mount();
 }
 
 	
- 	// responsive: [
-  //   {
-  //     // screens greater than >= 775px
-  //     breakpoint: 775,
-  //     settings: {
-  //       // Set to `auto` and provide item width to adjust to viewport
-  //       slidesToShow: 'auto',
-  //       slidesToScroll: 'auto',
-  //       itemWidth: 150,
-  //       duration: 0.25
-  //     }
-  //   },{
-  //     // screens greater than >= 1024px
-  //     breakpoint: 1024,
-  //     settings: {
-  //       slidesToShow: 2,
-  //       slidesToScroll: 1,
-  //       itemWidth: 150,
-  //       duration: 0.25
-  //     }
-  //   }
-  // ]
