2aef7cc65292636ec60e6a349b16c4fbacbea2e5
Konstantin Godunov <beqotemp>
2020-06-22 13:44:23 +0300

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+
+
+// JS
+import './scripts/menu.js'
+import './scripts/glide.js'
+import './scripts/range-slider.js'
+import './scripts/sticky.js'
+import './scripts/slick-slider'
+import './scripts/azazlyst'
+
+
+// Vue
+import Vue from 'vue/dist/vue.common.dev';
+import VCalendar from 'v-calendar/lib/components/date-picker.umd';
+new Vue({
+    el:'#app',
+    data:{
+    	date: '',
+    },
+    components: {
+        VCalendar
+    }
+})
+
---
+++ src/js/scripts/sticky.js
@@ -0,0 +1,13 @@
+// window.onscroll = function() {myFunction()};
+
+// var header = document.getElementById("catalog-nav");
+// var sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset > 72) {
+//   	console.log(header.offsetTop);
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
